aec9d7bade055773c67a99fd80e0452f008ec8cb
Aliyawar12 <aliyawar734@gmail.com>
2024-07-09 13:26:43 +0500

    Categories Add Page

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Constants\MyConstants; // Import your constants class
 
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name', 'type', 'icon_file', 'background_image', 'slug', 'status', 'description', 'created_by', 'updated_by'
+    ];
+
+    // Use constants directly from MyConstants class
+    public static function getCategoryTypes()
+    {
+        return MyConstants::CATEGORY_TYPES;
+    }
+
+    public static function getCategoryStatuses()
+    {
+        return MyConstants::CATEGORY_STATUSES;
+    }
 }
