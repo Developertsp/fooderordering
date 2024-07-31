b2344cdc5223a80e874e90241bfb710a8116e862
Aliyawar12 <aliyawar734@gmail.com>
2024-07-31 18:46:53 +0500

    seeder Update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,27 +2,23 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        $user = User::create([
-            'name'       => 'Software Manager',
-            'email'      => 'softwaremanager@gmail.com',
-            'role'       => 1,
-            'company_id' => NULL,
-            'password'   => Hash::make('12345678'),
-            'created_by' => 1,
+        $this->call([
+            UserTableSeeder::class,      
+            CategoriesTableSeeder::class,  
+            ProductsTableSeeder::class,   
+            CompaniesTableSeeder::class,   
+            MenusTableSeeder::class,       
         ]);
     }
 }
